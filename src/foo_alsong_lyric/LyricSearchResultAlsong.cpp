361ecce34b6ee5b0d41d72bb279e5789fb5fb646
Inseok Lee <dlunch@gmail.com>
2010-08-16 12:17:52 +0900

    Add license

--- src/foo_alsong_lyric/LyricSearchResultAlsong.cpp
+++ src/foo_alsong_lyric/LyricSearchResultAlsong.cpp
@@ -1,3 +1,20 @@
+/*
+* foo_alsong_lyric														
+* Copyright (C) 2007-2010 Inseok Lee <dlunch@gmail.com>
+*
+* This library is free software; you can redistribute it and/or modify it 
+* under the terms of the GNU Lesser General Public License as published 
+* by the Free Software Foundation; version 2.1 of the License.
+*
+* This library is distributed in the hope that it will be useful, 
+* but WITHOUT ANY WARRANTY; without even the implied warranty of 
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+* See the GNU Lesser General Public License for more details.
+*
+* You can receive a copy of the GNU Lesser General Public License from 
+* http://www.gnu.org/
+*/
+
 #include "stdafx.h"
 
 #include "LyricSearchResultAlsong.h"
